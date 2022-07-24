350819ca0d4541176e528eb5c177895c6035085f
Dylan Yung <dyung6@gatech.edu>
2022-07-24 10:52:36 -0400

    updated RAD for other augs

--- src/algorithms/rad.py
+++ src/algorithms/rad.py
@@ -1,13 +1,19 @@
-import numpy as np
-import torch
-import torch.nn as nn
-import torch.nn.functional as F
-from copy import deepcopy
-import utils
-import algorithms.modules as m
+import augmentations
 from algorithms.sac import SAC
 
 
 class RAD(SAC):
-	def __init__(self, obs_shape, action_shape, args):
-		super().__init__(obs_shape, action_shape, args)
+    def __init__(self, obs_shape, action_shape, args):
+        super().__init__(obs_shape, action_shape, args)
+        self.aug_func = augmentations.aug_to_func[args.data_aug]
+
+    def update(self, replay_buffer, L, step):
+        obs, action, reward, next_obs, not_done = replay_buffer.sample()
+        obs = self.aug_func(obs)
+        self.update_critic(obs, action, reward, next_obs, not_done, L, step)
+
+        if step % self.actor_update_freq == 0:
+            self.update_actor_and_alpha(obs, L, step)
+
+        if step % self.critic_target_update_freq == 0:
+            self.soft_update_critic_target()
