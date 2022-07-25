b7f2ac235d875b4f1186140dcc97b4cbdff0486a
Dylan Yung <dyung6@gatech.edu>
2022-07-25 19:22:32 -0400

    separated out aug and shift loss

--- src/algorithms/svea.py
+++ src/algorithms/svea.py
@@ -24,29 +24,44 @@ class SVEA(SAC):
             target_V = torch.min(target_Q1, target_Q2) - self.alpha.detach() * log_pi
             target_Q = reward + (not_done * self.discount * target_V)
 
-        if self.svea_alpha == self.svea_beta:
-            obs = utils.cat(obs, self.aug_func(obs.clone()))
-            action = utils.cat(action, action)
-            target_Q = utils.cat(target_Q, target_Q)
+        # if self.svea_alpha == self.svea_beta:
+        #     obs = utils.cat(obs, self.aug_func(obs.clone()))
+        #     action = utils.cat(action, action)
+        #     target_Q = utils.cat(target_Q, target_Q)
 
-            current_Q1, current_Q2 = self.critic(obs, action)
-            critic_loss = (self.svea_alpha + self.svea_beta) * (
-                F.mse_loss(current_Q1, target_Q) + F.mse_loss(current_Q2, target_Q)
-            )
-        else:
-            current_Q1, current_Q2 = self.critic(obs, action)
-            critic_loss = self.svea_alpha * (
-                F.mse_loss(current_Q1, target_Q) + F.mse_loss(current_Q2, target_Q)
-            )
+        #     current_Q1, current_Q2 = self.critic(obs, action)
+        #     critic_loss = (self.svea_alpha + self.svea_beta) * (
+        #         F.mse_loss(current_Q1, target_Q) + F.mse_loss(current_Q2, target_Q)
+        #     )
+        # else:
+        #     current_Q1, current_Q2 = self.critic(obs, action)
+        #     critic_loss = self.svea_alpha * (
+        #         F.mse_loss(current_Q1, target_Q) + F.mse_loss(current_Q2, target_Q)
+        #     )
 
-            obs_aug = self.aug_func(obs.clone())
-            current_Q1_aug, current_Q2_aug = self.critic(obs_aug, action)
-            critic_loss += self.svea_beta * (
-                F.mse_loss(current_Q1_aug, target_Q)
-                + F.mse_loss(current_Q2_aug, target_Q)
-            )
+        #     obs_aug = self.aug_func(obs.clone())
+        #     current_Q1_aug, current_Q2_aug = self.critic(obs_aug, action)
+        #     critic_loss += self.svea_beta * (
+        #         F.mse_loss(current_Q1_aug, target_Q)
+        #         + F.mse_loss(current_Q2_aug, target_Q)
+        #     )
+
+        current_Q1, current_Q2 = self.critic(obs, action)
+        critic_loss_shift = self.svea_alpha * (
+            F.mse_loss(current_Q1, target_Q) + F.mse_loss(current_Q2, target_Q)
+        )
+
+        obs_aug = self.aug_func(obs.clone())
+        current_Q1_aug, current_Q2_aug = self.critic(obs_aug, action)
+        critic_loss_aug = self.svea_beta * (
+            F.mse_loss(current_Q1_aug, target_Q) + F.mse_loss(current_Q2_aug, target_Q)
+        )
+
+        critic_loss = critic_loss_shift + critic_loss_aug
 
         if L is not None:
+            L.log("train_critic/shift_loss", critic_loss_shift, step)
+            L.log("train_critic/aug_loss", critic_loss_aug, step)
             L.log("train_critic/loss", critic_loss, step)
 
         self.critic_optimizer.zero_grad()
