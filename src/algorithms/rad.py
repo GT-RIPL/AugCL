04dddf12e1cd2baca2077b00628e1bedb6592f2f
Dylan Yung <dyung6@gatech.edu>
2022-07-31 23:43:33 -0400

    fixed rad to do original code

--- src/algorithms/rad.py
+++ src/algorithms/rad.py
@@ -10,6 +10,7 @@ class RAD(SAC):
     def update(self, replay_buffer, L, step):
         obs, action, reward, next_obs, not_done = replay_buffer.sample()
         obs = self.aug_func(obs)
+        next_obs = self.aug_func(next_obs)
         self.update_critic(obs, action, reward, next_obs, not_done, L, step)
 
         if step % self.actor_update_freq == 0:
