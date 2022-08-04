994d1ef7ba32a1306724cc88ab75939a96f8f165
Dylan Yung <dyung6@gatech.edu>
2022-08-04 02:20:32 -0400

    updated transfer to be curriculum

--- src/algorithms/curriculum/curriculum_backbone.py
+++ src/algorithms/curriculum/curriculum_backbone.py
@@ -1,7 +1,11 @@
 import augmentations
 from algorithms.sac import SAC
 
-class TransferBackbone(SAC):  # [K=1, M=1]
+
+class CurriculumBackbone(SAC):  # [K=1, M=1]
     def __init__(self, obs_shape, action_shape, args):
         super().__init__(obs_shape, action_shape, args)
         self.aug_func = augmentations.aug_to_func[args.transfer_data_augs]
+
+    def train_hard(self, x):
+        raise NotImplementedError("Hard train not implemented")
