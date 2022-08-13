d369356a03ff0cd775f126199ea1e65331ccb399
Dylan Yung <dyung6@gatech.edu>
2022-08-13 17:15:03 -0400

    added double augs

--- src/algorithms/curriculum_learning/curriculum.py
+++ src/algorithms/curriculum_learning/curriculum.py
@@ -6,7 +6,8 @@ import augmentations as augmentations
 class Curriculum(SAC):
     def __init__(self, obs_shape, action_shape, args):
         super().__init__(obs_shape, action_shape, args)
-        self.aug_func = augmentations.aug_to_func[args.data_aug]
+        self.aug_keys = args.data_aug.split("-")
+        self.aug_funcs = [augmentations.aug_to_func[key] for key in self.aug_keys]
 
     def update(self, replay_buffer: ReplayBuffer, L, step):
         obs, action, reward, next_obs, not_done = replay_buffer.sample()
