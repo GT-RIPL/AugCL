53f18c42795eaeadd5768fc82fc88004fc343e21
Dylan Yung <dyung6@gatech.edu>
2022-08-13 17:31:50 -0400

    fixed curriculum

--- src/algorithms/curriculum_learning/curriculum.py
+++ src/algorithms/curriculum_learning/curriculum.py
@@ -11,7 +11,7 @@ class Curriculum(SAC):
 
     def update(self, replay_buffer: ReplayBuffer, L, step):
         obs, action, reward, next_obs, not_done = replay_buffer.sample()
-        obs = self.aug_func(obs)
+        obs = self.aug_funcs(obs)
 
         self.update_critic(
             obs=obs,
