b1b27eeac1e8687996d4ecb0d2146af40557c374
Dylan Yung <dyung6@gatech.edu>
2022-08-16 21:30:09 -0400

    added method name saving

--- src/compile_csv.py
+++ src/compile_csv.py
@@ -5,8 +5,7 @@ import argparse
 
 def main(args):
     compiled_df = None
-    for dir_path in args.alg_dirs:
-        method_name = dir_path.split("/")[-2]
+    for dir_path, method_name in zip(args.alg_dirs, args.method_names):
         for seed in range(args.max_seed + 1):
             alg_seed_path = os.path.join(dir_path, f"seed_{seed}")
             csv_path = os.path.join(alg_seed_path, args.csv_file_name)
@@ -30,6 +29,7 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("--csv_name", default="No_name", type=str)
     parser.add_argument("--alg_dirs", "--list", nargs="+", required=True)
+    parser.add_argument("--method_names", "--list", nargs="+", required=True)
     parser.add_argument("--max_seed", default=1, type=int)
     parser.add_argument("--metric", default="episode_reward", type=str)
     parser.add_argument("--csv_file_name", default="train.csv", type=str)
