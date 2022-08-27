0c82651372f27644525bebb7aac1acaaf7139ef4
Dylan Yung <dyung6@gatech.edu>
2022-08-27 17:08:17 -0400

    fixed up stacked splice 2x jitter

--- src/augmentations.py
+++ src/augmentations.py
@@ -409,15 +409,19 @@ def stacked_splice_2x_conv(x, hue_thres=0, sat_thres=0, val_thres=0.6):
 def stacked_splice_2x_jitter(x, hue_thres=0, sat_thres=0, val_thres=0.6):
     """Applies a random conv2d, deviates slightly from https://arxiv.org/abs/1910.05396"""
     n, c, h, w = x.shape
+    sampled_hues = np.random.uniform(-0.5, 0.5, (n, 2))
+    sampled_saturs = np.random.uniform(0, 2, (n, 2))
     for i in range(n):
         temp_x = x[i : i + 1].reshape(-1, 3, h, w) / 255.0
         mask = create_hsv_mask(
             temp_x, hue_thres=hue_thres, sat_thres=sat_thres, val_thres=val_thres
         )
-        model = TF.ColorJitter(0, 0, 0, 0.5)
-        model_2 = TF.ColorJitter(0, 0, 0, 0.5)
-        out = model(temp_x)
-        out_2 = model_2(temp_x)
+        hues = sampled_hues[i]
+        saturs = sampled_saturs[i]
+        out = TF.functional.adjust_hue(temp_x, hues[0])
+        out = TF.functional.adjust_saturation(out, saturs[0])
+        out_2 = TF.functional.adjust_hue(temp_x, hues[1])
+        out_2 = TF.functional.adjust_saturation(out_2, saturs[1])
         out[mask] = out_2[mask]
         total_out = out if i == 0 else torch.cat([total_out, out], axis=0)
     return total_out.reshape(n, c, h, w) * 255.0
