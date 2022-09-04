3c9b64e89eb97fbc13859a0c189640cd4618ad7c
Dylan Yung <dyung6@gatech.edu>
2022-09-04 16:48:28 -0400

    removed map to cpu

--- src/interruptible_utils.py
+++ src/interruptible_utils.py
@@ -58,9 +58,8 @@ def is_requeued():
 
 
 def requeue_load_agent_and_replay_buffer(replay_buffer: ReplayBuffer):
-    state_dict = torch.load(STATE_FILE, map_location="cpu")
+    state_dict = torch.load(STATE_FILE)
     replay_buffer.load(BUFFER_FOLDER)
-    state_dict["agent"] = state_dict["agent"].cuda()
     return state_dict["agent"], state_dict["step"]
 
 
