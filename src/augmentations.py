95eb7fa97af0ba510ccf97169f0fb7e3f6396ee0
Dylan Yung <dyung6@gatech.edu>
2022-09-06 15:23:44 -0400

    added DrQ2 random shift

--- src/augmentations.py
+++ src/augmentations.py
@@ -334,6 +334,28 @@ def splice(x, hue_thres=0, sat_thres=0, val_thres=0.6):
     return x_rgb.reshape(n, c, h, w) * 255.0
 
 
+def DrQ2_random_shift(x, pad=4):
+    n, _, h, w = x.size()
+    assert h == w
+    padding = tuple([pad] * 4)
+    x = F.pad(x, padding, "replicate")
+    eps = 1.0 / (h + 2 * pad)
+    arange = torch.linspace(
+        -1.0 + eps, 1.0 - eps, h + 2 * pad, device=x.device, dtype=x.dtype
+    )[:h]
+    arange = arange.unsqueeze(0).repeat(h, 1).unsqueeze(2)
+    base_grid = torch.cat([arange, arange.transpose(1, 0)], dim=2)
+    base_grid = base_grid.unsqueeze(0).repeat(n, 1, 1, 1)
+
+    shift = torch.randint(
+        0, 2 * pad + 1, size=(n, 1, 1, 2), device=x.device, dtype=x.dtype
+    )
+    shift *= 2.0 / (h + 2 * pad)
+
+    grid = base_grid + shift
+    return F.grid_sample(x, grid, padding_mode="zeros", align_corners=False)
+
+
 def splice_mix_up(x, hue_thres=0, sat_thres=0, val_thres=0.6):
     # 0.6 val for video_hard, 0.4 for video_easy
     global data_iter
@@ -635,4 +657,5 @@ aug_to_func = {
     "random_perspective": random_perspective,
     "random_resize_crop": random_resize_crop,
     "random_rescale": random_rescale,
+    "DrQ2_random_shift": DrQ2_random_shift,
 }
