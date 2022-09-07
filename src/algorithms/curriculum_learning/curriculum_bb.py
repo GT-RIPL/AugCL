a3a6e7056650223ff5beea5eeaecda635d0b10d8
Dylan Yung <dyung6@gatech.edu>
2022-09-07 03:18:39 -0400

    no shift for curr bb

--- src/algorithms/curriculum_learning/curriculum_bb.py
+++ src/algorithms/curriculum_learning/curriculum_bb.py
@@ -1,9 +1,9 @@
 from augmentations import random_shift
-from algorithms.curriculum_learning.curriculum_double import Curriculum_Double
+from algorithms.curriculum_learning.curriculum_2x_nu_actor import Curriculum_2x_Nu_Actor
 from utils import ReplayBuffer
 
 
-class Curriculum_BB(Curriculum_Double):
+class Curriculum_BB(Curriculum_2x_Nu_Actor):
     def __init__(self, obs_shape, action_shape, args):
         super().__init__(obs_shape, action_shape, args)
 
@@ -24,7 +24,7 @@ class Curriculum_BB(Curriculum_Double):
         )
 
         if step % self.actor_update_freq == 0:
-            self.update_actor_and_alpha(obs=obs, L=L, step=step)
+            self.update_actor_and_alpha(obs_shift=obs, obs_aug=obs_aug, L=L, step=step)
 
         if step % self.critic_target_update_freq == 0:
             self.soft_update_critic_target()
